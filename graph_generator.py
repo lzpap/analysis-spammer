dae9feeac9da042d70e5a6d37bed3a47f26dc0e4
Levente Pap <levente.pap@iota.org>
2020-04-17 20:36:43 +0200

    Improve graph generator, add patterns

--- graph_generator.py
+++ graph_generator.py
@@ -1,11 +1,108 @@
 from networkx.generators.random_graphs import erdos_renyi_graph
 import random
 import sys
+from bfs import Graph
+
+  
+
 
 n = int(sys.argv[1])
-p = 8.0/n
-g = erdos_renyi_graph(n, p)
 
+class Result:
+    def __init__(self, nodes, links):
+        self.nodes = nodes
+        self.edges = links
+
+class Node:
+    """
+
+    """
+    def __init__(self, id_):
+        self.id = id_
+        self.inn = []
+        self.out = []
+        self.max_num = 4
+    
+    def add_in(self, n_id):
+        if n_id == self.id:
+            return 0, 0
+        if self.num_in() >= 4:
+            return 0, 0
+        self.inn.append(n_id)
+        return 1, n_id
+    
+    def add_out(self, n_id):
+        if n_id == self.id:
+            return 0, 0
+        if self.num_out() >= 4:
+            return 0 ,0
+        self.out.append(n_id)
+        return 1, n_id
+    
+    def num_in(self):
+        return len(self.inn)
+    
+    def num_out(self):
+        return len(self.out)
+
+    
+
+def gen_shimmer(n: int):
+    nodes = [ Node(x ) for x in range(n)] # start with 1
+
+    nodes_dict = {}
+
+    for node in nodes:
+        nodes_dict[node.id] = node
+
+    finished_nodes = []
+
+    for x in range(n-10):
+        x = x
+        node = nodes_dict[x]
+        for i in range(4 - node.num_in()):
+            added = 0
+            nid = 0
+            while added != 1:
+                added, nid = node.add_in(random.choice(list(nodes_dict.keys())))
+            nodes_dict[nid].add_out(node.id)
+        for i in range(4 - node.num_out()):
+            added = 0
+            nid = 0
+            while added != 1:
+                added, nid = node.add_out(random.choice(list(nodes_dict.keys())))
+            nodes_dict[nid].add_in(node.id)
+
+        finished_nodes.append(node)
+        del nodes_dict[node.id]
+
+    edges = []
+    nodes_ = []
+    for node in finished_nodes:
+        for in_ in node.inn:
+            if not (node.id, in_) in edges:
+                edges.append((node.id, in_))
+        for out in node.out:
+            if not (out, node.id) in edges:
+                edges.append((out, node.id))
+        nodes_.append(node.id)
+
+    g = Graph()
+    for edge in edges:
+        g.addEdge(edge[0], edge[1])
+
+    bfs_ordered = []
+    g.BFS(0, bfs_ordered)
+
+    result = Result([x.id for x in nodes],edges)
+
+    return result
+
+
+def gen_erdos(n: int):
+    p = 8.0/n
+    return erdos_renyi_graph(n, p)
+g = gen_shimmer(n)
 #print(g.nodes)
 print("Number of nodes: " + str(len(g.nodes)))
 # [0, 1, 2, 3, 4, 5]
@@ -22,6 +119,7 @@ def randString(length=64):
 
 node_ids = []
 links = {}
+
 
 for node in g.nodes:
     node_ids.append(randString())
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"crypto/sha256"
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -14,6 +15,9 @@ import (
 	"crypto/rand"
 )
 
+var additionRate = flag.Float64("nps", 10.0, "Amount of nodes to add each second")
+var pattern = flag.String("pattern", "distribute", "Pattern for spamming")
+
 // A heartbeat packet
 type Packet struct {
 	OwnID       []byte
@@ -21,7 +25,19 @@ type Packet struct {
 	InboundIDs  [][]byte
 }
 
+var methods = map[string]func([]string, map[string][]string){
+	"flood":         flood,
+	"flood-reverse": floodReverse,
+	"distribute":    distribute,
+}
+
+// Scenario ideas:
+// 1. Distribute reporting times over 5 seconds evenly
+// 2. Start sending heartbeats in reverse order (nodes)
+// 3. Start sending in random order
+
 func main() {
+	flag.Parse()
 	nodes := readNodes()
 	links := readLinks()
 
@@ -38,6 +54,20 @@ func main() {
 		}
 	}
 	fmt.Println("Linkcount is: ", linkCounter)
+	actualPattern := methods[*pattern]
+	actualPattern(nodes, links)
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("I'm alive.")
+		}
+	}
+}
+
+// flood() floodes the server with heartbeats starting from the first node.
+// Heartbeat messages are sent immediately after one another.
+func flood(nodes []string, links map[string][]string) {
 	for _, node := range nodes {
 		own := []byte(node)
 
@@ -69,13 +99,84 @@ func main() {
 		packet := Packet{OwnID: own, OutboundIDs: out, InboundIDs: in}
 
 		go sendPacket(packet)
-		//time.Sleep(30 * time.Millisecond)
 	}
-	ticker := time.NewTicker(5 * time.Second)
-	for {
-		select {
-		case <-ticker.C:
-			fmt.Println("I'm alive.")
+}
+
+// floodReverse() floodes the server with heartbeats starting from the last node.
+// Heartbeat messages are sent immediately after one another.
+func floodReverse(nodes []string, links map[string][]string) {
+	for i := len(nodes) - 1; i > 0; i-- {
+		node := nodes[i]
+		own := []byte(node)
+
+		out := make([][]byte, len(links[node]))
+		for i, neighbor := range links[node] {
+			out[i] = []byte(neighbor)
+		}
+
+		inSize := 0
+
+		for _, link := range links {
+			for _, neighbor := range link {
+				if neighbor == node {
+					inSize++
+				}
+			}
+		}
+
+		in := make([][]byte, inSize)
+		inSize = 0
+		for key, value := range links {
+			for _, neighbor := range value {
+				if neighbor == node {
+					in[inSize] = []byte(key)
+					inSize++
+				}
+			}
+		}
+		packet := Packet{OwnID: own, OutboundIDs: out, InboundIDs: in}
+
+		go sendPacket(packet)
+	}
+}
+
+func distribute(nodes []string, links map[string][]string) {
+	delay := time.Duration(float64(time.Second) / (*additionRate))
+	fmt.Println("Delay is: ", delay)
+	fmt.Println("Node addition rate: ", *additionRate, " node(s)/sec")
+	for i, node := range nodes {
+		own := []byte(node)
+
+		out := make([][]byte, len(links[node]))
+		for i, neighbor := range links[node] {
+			out[i] = []byte(neighbor)
+		}
+
+		inSize := 0
+
+		for _, link := range links {
+			for _, neighbor := range link {
+				if neighbor == node {
+					inSize++
+				}
+			}
+		}
+
+		in := make([][]byte, inSize)
+		inSize = 0
+		for key, value := range links {
+			for _, neighbor := range value {
+				if neighbor == node {
+					in[inSize] = []byte(key)
+					inSize++
+				}
+			}
+		}
+		packet := Packet{OwnID: own, OutboundIDs: out, InboundIDs: in}
+
+		go sendPacket(packet)
+		if i > len(nodes)/5 {
+			time.Sleep(time.Duration(delay))
 		}
 	}
 }
@@ -83,13 +184,13 @@ func main() {
 func sendPacket(packet Packet) {
 	ticker := time.NewTicker(5 * time.Second)
 	conn, err := net.Dial("tcp", "127.0.0.1:188")
+	//i := 0
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	for {
 		select {
 		case <-ticker.C:
-
 			data, err := packet.Marshal()
 			if err != nil {
 				fmt.Println(err)
@@ -97,6 +198,7 @@ func sendPacket(packet Packet) {
 			_, _ = conn.Write(data)
 		}
 	}
+	// Deliberately keep the connection open until timeout on server (10s)
 	conn.Close()
 
 }
